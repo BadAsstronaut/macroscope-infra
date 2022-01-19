5f4f07c86cfbee6d9652e4f9d526133772a2232b
Mickey <kyle_mickey@trimble.com>
2022-01-18 17:07:32 -0700

    S3 processing logic stubbed out

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Macroscope Infrastructure
-
--- handle_macro_sns_event/main.py
+++ handle_macro_sns_event/main.py
@@ -2,25 +2,72 @@
 Handler for SNS topic subscription
 '''
 
+from datetime import datetime
 import io
 import json
+import os
 
+import boto3
 import pandas as pd
 
 
-def convert_csv_to_parquet(csv: io.BaseIO) -> io.BaseIO:
+local_session = boto3.Session()
+local_s3_client = local_session.client('s3')
+
+SOURCE_S3_BUCKET = os.environ['SOURCE_S3_BUCKET']
+SOURCE_S3_BUCKET_ROLE = os.environ['SOURCE_S3_BUCKET_ROLE']
+TARGET_S3_BUCKET = os.environ['TARGET_S3_BUCKET']
+
+def dataframe_to_parquet(dataframe: pd.DataFrame) -> io.BaseIO:
     '''
     Uses pd to convert a BaseIO type from csv to parquet
     '''
-    dataframe = pd.read_csv(csv)
     result = io.BytesIO()
     dataframe.to_parquet(result)
     result.seek(0)
     return result
 
 
-def time_interval(iso_time: str) -> str:
-    '''  '''
+def save_target_s3(file: io.BaseIO, path: str) -> None:
+    ''' Saves the file to the target S3 path on the local account bucket '''
+    local_s3_client.put_object(Body=file,
+                               Bucket=TARGET_S3_BUCKET,
+                               Key=path)
+
+
+def get_s3_object(bucket: str, path: str, session: boto3.Session) -> io.BaseIO:
+    '''
+    Gets the S3 object
+    '''
+    s3_client = session.client('s3')
+
+    res = s3_client.get_object(Bucket=bucket,
+                               Key=path)
+
+    return io.BytesIo(res.body.read())
+
+
+def get_cross_account_session() -> boto3.Session:
+    '''
+    Reference: https://aws.amazon.com/premiumsupport/knowledge-center/lambda-function-assume-iam-role/
+    '''
+    sts = boto3.client('sts')
+    acct = sts.assume_role(RoleArn=SOURCE_S3_BUCKET_ROLE,
+                           RoleSessionName='mickey-macroscope-interview')
+    creds = acct['Credentials']
+
+    return boto3.Session(aws_access_key_id=creds['AccessKeyId'],
+                         aws_secret_access_key=creds['SecretAccessKey'],
+                         aws_session_token=creds['SessionToken'])
+
+
+def process_s3_obj(key: str, session: boto3.Session) -> None:
+    ''' Handles processing the s3 file and uploading the updated result  '''
+    raw_file = get_s3_object(SOURCE_S3_BUCKET, key, session)
+    dataframe = pd.read_csv(raw_file)
+    dataframe.resample('5min').sum()
+    pq_data = dataframe_to_parquet(dataframe)
+    save_target_s3(pq_data, f'/{datetime.now().isoformat()}.parquet')
 
 
 def handler(event: dict, _) -> None:
@@ -35,8 +82,11 @@ def handler(event: dict, _) -> None:
          "/infra-eng/<your-specific-subpath>/input/2021-01-1T12:03:11-06:00.csv"]}
     '''
 
-    try:
-        sns_messages = json.loads(event['Records'][0]['Sns']['Message'])
+    x_acct_session = get_cross_account_session()
 
+    try:
+        s3_object_paths = json.loads(event['Records'][0]['Sns']['Message'])['s3_objects']
     except KeyError as key_ex:
         raise Exception('Sns message format did not conform to expected payload schema') from key_ex
+
+    _ = [process_s3_obj(el, x_acct_session) for el in s3_object_paths]
